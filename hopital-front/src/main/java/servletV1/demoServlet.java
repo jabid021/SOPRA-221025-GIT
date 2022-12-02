b674c9a27f5cc5fddd3be89c89e2979c7a6b8a43
Jordan <Jordanabid@gmail.com>
2022-12-02 11:10:52 +0100

    hopital front

---
+++ hopital-front/src/main/java/servletV1/demoServlet.java
@@ -0,0 +1,46 @@
+package servletV1;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DAOPatient;
+import model.Patient;
+
+@WebServlet("/demo")
+public class demoServlet extends HttpServlet {
+	
+     
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		DAOPatient daoP = new DAOPatient();
+		List<Patient> patients = daoP.findAll();
+		
+		response.getWriter().println("<html>");
+			response.getWriter().println("<body>");
+				response.getWriter().println("<table>");
+					response.getWriter().println("<tr>");
+					response.getWriter().println("<th>ID</th><th>Nom</th><th>Prenom</th>");
+					response.getWriter().println("</tr>");
+					for(Patient p : patients) 
+					{
+						response.getWriter().println("<tr><td>"+p.getId()+"</td><td>"+p.getNom()+"</td><td>"+p.getPrenom()+"</td></tr>");
+					}
+					
+				response.getWriter().println("</table>");
+			response.getWriter().println("</body>");
+		response.getWriter().println("</html>");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+
+}
