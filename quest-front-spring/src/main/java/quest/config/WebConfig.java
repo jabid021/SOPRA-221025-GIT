3e207b349b77861bef8550d590a0443049100cf6
Eric <eric>
2022-12-15 10:55:10 +0100

    quest-front-spring v0

---
+++ quest-front-spring/src/main/java/quest/config/WebConfig.java
@@ -0,0 +1,35 @@
+package quest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc // Activation des annotations Spring Web : @Controller, @RestController,
+				// @RequestMapping, ...
+@ComponentScan("quest.web") // On précise dans quel(s) package(s) se situe(nt) les controlleurs
+public class WebConfig implements WebMvcConfigurer {
+
+	// On exclut certains mappings de la recherche de controller => accès direct
+	// fichier
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+	}
+
+	// On configure le moteur de template (JSP/JSTL) pour Spring
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+}
---
+++ quest-front-spring/src/main/java/quest/web/HomeController.java
@@ -0,0 +1,5 @@
+package quest.web;
+
+public class HomeController {
+
+}
