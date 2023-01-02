07a6e23ddc94337e97e9430071b076ef0d819e33
EricSemantik <eric@semantik.fr>
2023-01-02 16:06:45 +0100

    quest-spring-rest-full v5

--- quest-spring-rest-full/src/main/java/quest/web/StagiaireResource.java
+++ quest-spring-rest-full/src/main/java/quest/web/StagiaireResource.java
@@ -1,15 +1,19 @@
 package quest.web;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -67,5 +71,39 @@ public class StagiaireResource {
 		stagiaire = daoStagiaire.save(stagiaire);
 
 		return stagiaire;
+	}
+
+	@PatchMapping("/{id}")
+	public Stagiaire partialUpdate(@PathVariable Integer id, @RequestBody Map<String, Object> fields) {
+		Optional<Stagiaire> optStagiaire = daoStagiaire.findById(id);
+
+		if (optStagiaire.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+
+		final Stagiaire stagiaire = optStagiaire.get();
+
+//		if(fields.containsKey("nom")) {
+//			stagiaire.setNom((String) fields.get("nom"));
+//		}
+//		
+//		if(fields.containsKey("prenom")) {
+//			stagiaire.setPrenom((String) fields.get("prenom"));
+//		}
+//
+//		if(fields.containsKey("email")) {
+//			stagiaire.setEmail((String) fields.get("email"));
+//		}
+
+		fields.forEach((key, value) -> {
+			Field field = ReflectionUtils.findField(Stagiaire.class, key);
+			if (field == null) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Propriété : " + key + "non trouvée");
+			}
+			ReflectionUtils.makeAccessible(field);
+			ReflectionUtils.setField(field, stagiaire, value);
+		});
+
+		return daoStagiaire.save(stagiaire);
 	}
 }
--- quest-spring-rest-full/src/main/java/quest/web/old/StagiaireOldResource.java
+++ quest-spring-rest-full/src/main/java/quest/web/old/StagiaireOldResource.java
@@ -1,6 +1,8 @@
 package quest.web.old;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -9,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -78,5 +82,43 @@ public class StagiaireOldResource {
 		
 		return new ResponseEntity<>(stagiaire, HttpStatus.OK);
 	}
+	
+	@PatchMapping("/{id}")
+	public ResponseEntity<Stagiaire> partialUpdate(@PathVariable Integer id, @RequestBody Map<String, Object> fields) {
+		Optional<Stagiaire> optStagiaire = daoStagiaire.findById(id);
+		
+		if(optStagiaire.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		
+		final Stagiaire stagiaire = optStagiaire.get();
+				
+//		if(fields.containsKey("nom")) {
+//			stagiaire.setNom((String) fields.get("nom"));
+//		}
+//		
+//		if(fields.containsKey("prenom")) {
+//			stagiaire.setPrenom((String) fields.get("prenom"));
+//		}
+//
+//		if(fields.containsKey("email")) {
+//			stagiaire.setEmail((String) fields.get("email"));
+//		}
+		
+		fields.forEach((key, value) -> {
+			Field field = ReflectionUtils.findField(Stagiaire.class , key);
+			if(field == null) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Propriété : " + key + "non trouvée" );
+			}
+			ReflectionUtils.makeAccessible(field);
+			ReflectionUtils.setField(field, stagiaire, value);
+		});
+		
+		
+		Stagiaire stagiaireResult = daoStagiaire.save(stagiaire);
+		
+		return new ResponseEntity<>(stagiaireResult, HttpStatus.OK);
+	}
+	
 
 }
