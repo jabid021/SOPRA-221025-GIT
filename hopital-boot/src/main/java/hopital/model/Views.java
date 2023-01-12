296b5116922f7535191b5d6baf3107ea48f55560
Sammydoo <huot.clement86@gmail.com>
2023-01-12 11:46:48 +0100

    viewConnexion

--- hopital-boot/src/main/java/hopital/model/Views.java
+++ hopital-boot/src/main/java/hopital/model/Views.java
@@ -1,7 +1,9 @@
 package hopital.model;
 
 public class Views {
-	public static class ViewBase {
-		
-	}
+	public static class ViewBase {}
+	
+	public static class ViewConnexion extends ViewBase {}
+	
+	public static class ViewConnexionDetail extends ViewConnexion{}
 }
--- hopital-boot/src/main/java/hopital/web/MedecinRessource.java
+++ hopital-boot/src/main/java/hopital/web/MedecinRessource.java
@@ -1,5 +1,22 @@
 package hopital.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hopital.repository.ICompteRepository;
+import hopital.repository.IPatientRepository;
+import hopital.repository.IVisiteRepository;
+
+@RestController
+@RequestMapping("/medecin")
+@CrossOrigin("*")
 public class MedecinRessource {
+	
+	@Autowired
+	private ICompteRepository daoCompte;
+	private IPatientRepository daoPatient;
+	private IVisiteRepository daoVisite;
 
 }
