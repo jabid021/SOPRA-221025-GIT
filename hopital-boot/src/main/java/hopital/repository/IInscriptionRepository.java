9ef6e72378aca68ed3605c04c1e58cfacbbd35c0
Khamad Taher <attaherikhiri@yahoo.fr>
2023-01-12 12:21:47 +0100

    fixed inscription

---
+++ hopital-boot/src/main/java/hopital/repository/IInscriptionRepository.java
@@ -0,0 +1,9 @@
+package hopital.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import hopital.model.Inscription;
+
+public interface IInscriptionRepository extends JpaRepository<Inscription,Integer>{
+	
+}
