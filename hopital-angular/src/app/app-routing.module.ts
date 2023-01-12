24906a8b7017dbaa44c0aa248eb67e4fdae94b77
Pierre HANZAL <pierre.hanz@gmail.com>
2023-01-12 14:56:51 +0100

    Ajout des routes + model.ts

--- hopital-angular/src/app/app-routing.module.ts
+++ hopital-angular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { MedecinComponent } from './medecin/medecin.component';
+import { PauseComponent } from './pause/pause.component';
+import { SecretaireComponent } from './secretaire/secretaire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: ConnexionComponent, pathMatch: 'full'},
+  {path: "medecin", component: MedecinComponent},
+  {path: "secretaire", component: SecretaireComponent},
+  {path: "pause", component: PauseComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
