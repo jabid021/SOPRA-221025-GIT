925320306d6efc2621378fc6f9e0e8407285c50e
otmne <otmkne120@gmail.com>
2023-01-12 15:33:50 +0100

    Auto stash before merge of "groupe-des-bg-hopital" and "origin/groupe-des-bg-hopital"
    l

---
+++ bg-hopital-boot/src/app/secretaire/secretaire.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secretaire',
+  templateUrl: './secretaire.component.html',
+  styleUrls: ['./secretaire.component.scss']
+})
+export class SecretaireComponent {
+
+}
